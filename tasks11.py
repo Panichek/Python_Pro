0e794ca8a4c95297efc5be20d728d09e9f9f9e79
Panichek <60155414+Panichek@users.noreply.github.com>
2023-06-28 10:39:05 +0300

    Add files via upload

--- tasks11.py
+++ tasks11.py
@@ -57,10 +57,6 @@ class Article:
         self.text = text
 
 
-class Articles():
-    articles = []
-
-
 class PapperArticle(Article):
     def __init__(self,
                  title,
@@ -101,6 +97,29 @@ class NewsArticle(Article):
         self.news = news
 
 
+def get_article(article_type):
+    available_articles = {"1": PapperArticle,
+                          "2": SiteArticle,
+                          "3": BlogArticle,
+                          "4": NewsArticle}
+    return available_articles.get(article_type)
+
+
+articles = []
+
+
+def menu():
+    choice = input("1 papper 2 site 3 blog 4 news:")
+    article_class = get_article(choice)
+    if article_class:
+        article = article_class("", "", "", "")
+        articles.append(article)
+
+
+while True:
+    menu()
+    print(articles)
+'''   
 def menu():
     choice = input("1 papper 2 site 3 blog 4 news:")
     if choice == "1":
@@ -114,7 +133,7 @@ def menu():
     else:
         article = None
     if article:
-        Articles.articles.append(article)
-
+        articles.append(article)
 
 menu()
+'''
